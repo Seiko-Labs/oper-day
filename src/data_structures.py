23feda010cda7e9a35bf3ca7ec28831f3667d31b
Alikhan Bissakov <bissakov@yandex.ru>
2023-01-24 12:55:38 +0600

    Moved DateInfo to data_structures.py

--- src/data_structures.py
+++ src/data_structures.py
@@ -13,3 +13,28 @@ class Credentials:
 class Process:
     name: str
     path: str
+
+
+@dataclass
+class DateInfo:
+    date: datetime.date or str
+    is_work_day: bool = None
+    date_str: str = None
+    weekday: int = None
+    weekday_str: str = None
+    next_date_str: str = None
+    prev_date_str: str = None
+
+    def __post_init__(self):
+        if isinstance(self.date, str):
+            self.date = datetime.date.fromisoformat(self.date)
+        self.date_str = self.date.strftime('%d.%m.%y')
+        self.weekday = self.date.weekday() + 1
+        self.weekday_str = self.date.strftime('%A')
+        self.next_date_str = (self.date + datetime.timedelta(days=1)).strftime('%d.%m.%y')
+
+
+class WorkStatus:
+    HOLIDAY = 0
+    WORK = 1
+    LONG = 2
