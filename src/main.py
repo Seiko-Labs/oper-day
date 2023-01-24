f33f1991943e38e45505f892f04ad53d31fa692d
Alikhan Bissakov <bissakov@yandex.ru>
2023-01-24 14:29:28 +0600

    Optimized .env interaction

--- src/main.py
+++ src/main.py
@@ -10,13 +10,8 @@ def main(env: str) -> None:
     warnings.simplefilter(action='ignore', category=UserWarning)
     dotenv.load_dotenv()
 
-    process_name = 'COLVIR'
-    if env == 'dev':
-        colvir_usr, colvir_psw = os.getenv('COLVIR_USR_DEV'), os.getenv('COLVIR_PSW_DEV')
-        process_path = r'C:\CBS_T_new\COLVIR.EXE'
-    else:
-        colvir_usr, colvir_psw = os.getenv('COLVIR_USR_PROD'), os.getenv('COLVIR_PSW_PROD')
-        process_path = r'C:\CBS_R\COLVIR.EXE'
+    colvir_usr, colvir_psw = os.getenv(f'COLVIR_USR_{env}'), os.getenv(f'COLVIR_PSW_{env}')
+    process_name, process_path = 'COLVIR', os.getenv('COLVIR_PROCESS_PATH')
 
     credentials: Credentials = Credentials(usr=colvir_usr, psw=colvir_psw)
     process: Process = Process(name=process_name, path=process_path)
