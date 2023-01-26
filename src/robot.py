3d87b80ecf34df57f8b42c3400392bd1ab58fca6
Alikhan Bissakov <bissakov@yandex.ru>
2023-01-26 14:56:24 +0600

    Fixed string encoding and added session

--- src/robot.py
+++ src/robot.py
@@ -1,25 +1,26 @@
 from typing import List
 import dotenv
+import requests
 from bot_notification import TelegramNotifier
 from colvir import Colvir
 from data_structures import Credentials, Process, DateInfo, WorkStatus, RobotWorkTime
 from utils import Utils
 from work_calendar import CalendarScraper
 from datetime import datetime, timedelta
-from datetime import date
 
 
 class Robot:
     def __init__(self, credentials: Credentials, process: Process, notifier: TelegramNotifier,
-                 _date: datetime.date = datetime.now().date()) -> None:
+                 session: requests.Session, today: datetime.date = datetime.now().date()) -> None:
         self.restricted_pids: List[int] = []
         dotenv.load_dotenv()
         self.utils = Utils()
+        self.session = session
         self.args = {
             'credentials': credentials,
             'process': process,
             'notifier': notifier,
-            'today': DateInfo(date=_date),
+            'today': DateInfo(date=today),
             'robot_time': RobotWorkTime(start=datetime.now())
         }
 
@@ -27,23 +28,24 @@ class Robot:
         today = self.args['today']
         scraper = CalendarScraper(
             year=today.date.year,
-            backup_file=fr'C:\Users\robot.ad\Desktop\oper_day\resourses\{today.date.year}.html'
+            backup_file=fr'C:\Users\robot.ad\Desktop\oper_day\resourses\{today.date.year}.html',
+            session=self.session
         )
         date_infos: List[DateInfo] = scraper.date_infos
 
         work_status = scraper.get_work_status(today=today.date, dates=date_infos)
 
         if work_status == WorkStatus.LONG:
-            self.args['notifier'].send_notification('������� ����. ����� ����������� ������.')
             self.args['today'] = DateInfo(date=today.date - timedelta(days=1), is_work_day=False)
 
         return True if work_status in (WorkStatus.WORK, WorkStatus.LONG) else False
 
     def run(self) -> None:
         if not self.is_work_day():
-            self.args['notifier'].send_notification(message='������� ��� �������� ����. ����� ����������� ������.')
+            self.args['notifier'].send_notification(message='Не рабочий день. Завершаем работу.')
             return
 
         self.utils.kill_all_processes(proc_name='COLVIR', restricted_pids=self.restricted_pids)
         colvir: Colvir = Colvir(**self.args)
         colvir.run()
+        self.session.close()
