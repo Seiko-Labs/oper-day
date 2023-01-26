cdc44694efb007b4d138e6143e3d3c8c8b4af9fe
Alikhan Bissakov <bissakov@yandex.ru>
2023-01-26 14:55:29 +0600

    Added session arg

--- src/bot_notification.py
+++ src/bot_notification.py
@@ -1,24 +1,17 @@
 import os
-import dotenv
 import requests
 from requests.adapters import HTTPAdapter
 
 
 class TelegramNotifier:
-    def __init__(self, chat_id: str, retries: int = 5):
+    def __init__(self, chat_id: str, session: requests.Session, retries: int = 5):
         token: str = os.getenv('TOKEN')
         chat_id: str = chat_id
         self.api_url = f'https://api.telegram.org/bot{token}/sendMessage'
         self.api_params = {'chat_id': chat_id, 'parse_mode': 'Markdown'}
         self.retries = retries
-        self.session = requests.Session()
+        self.session = session
         self.session.mount("http://", HTTPAdapter(max_retries=self.retries))
 
     def send_notification(self, message: str) -> requests.models.Response:
         return self.session.post(self.api_url, params=self.api_params, json={'text': message})
-
-
-if __name__ == '__main__':
-    dotenv.load_dotenv()
-    notifier = TelegramNotifier()
-    notifier.send_notification('Hello, world!')
