28c86c2bd21bc1470be9f96a8a4a6f8dc2ddcc45
Alikhan Bissakov <bissakov@yandex.ru>
2023-02-10 15:35:34 +0600

    Telegram Bot optimizations

--- src/bot_notification.py
+++ src/bot_notification.py
@@ -5,13 +5,16 @@ from requests.adapters import HTTPAdapter
 
 class TelegramNotifier:
     def __init__(self, chat_id: str, session: requests.Session, token: str = None, retries: int = 5):
-        token: str = os.getenv('TOKEN') if not token else token
-        chat_id: str = chat_id
-        self.api_url = f'https://api.telegram.org/bot{token}/sendMessage'
+        self.token: str = os.getenv('TOKEN') if not token else token
         self.api_params = {'chat_id': chat_id, 'parse_mode': 'Markdown'}
         self.retries = retries
         self.session = session
-        self.session.mount("http://", HTTPAdapter(max_retries=self.retries))
+        self.session.mount('http://', HTTPAdapter(max_retries=self.retries))
 
-    def send_notification(self, message: str) -> requests.models.Response:
-        return self.session.post(self.api_url, params=self.api_params, json={'text': message})
+    def send_message(self, message: str, is_document: bool = False) -> requests.models.Response:
+        message_type = 'sendDocument' if is_document else 'sendMessage'
+        api_url = f'https://api.telegram.org/bot{self.token}/{message_type}'
+        args = {'url': api_url, 'params': self.api_params, 'json': {'text': message}}
+        if is_document:
+            args['files'] = {'document': message}
+        return self.session.post(**args)
