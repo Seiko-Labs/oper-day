901636705242f9748ddc9b44c9b2fceaa327f8eb
Alikhan Bissakov <bissakov@yandex.ru>
2023-02-15 12:29:16 +0600

    Update robot.py

--- src/robot.py
+++ src/robot.py
@@ -1,60 +1,99 @@
-from typing import List
+import os
+from datetime import datetime, timedelta
+from typing import List, Dict, Any
 import dotenv
+import oracledb
 import requests
-from bot_notification import TelegramNotifier
 from colvir import Colvir
-from data_structures import Credentials, Process, DateInfo, WorkStatus, RobotWorkTime
+from data_structures import Notifiers, Credentials, Process, DateInfo, WorkStatus, RobotWorkTime
 from utils import Utils
 from work_calendar import CalendarScraper
-from datetime import datetime, timedelta
 
 
 class Robot:
-    def __init__(self, credentials: Credentials, process: Process, notifier: TelegramNotifier,
+    def __init__(self, credentials: Credentials, process: Process, notifiers: Notifiers,
                  session: requests.Session, today: datetime.date = datetime.now().date()) -> None:
         self.restricted_pids: List[int] = []
         dotenv.load_dotenv()
         self.utils = Utils()
         self.session = session
-        self.args = {
-            'credentials': credentials,
-            'process': process,
-            'notifier': notifier,
-            'today': DateInfo(date=today),
-            'robot_time': RobotWorkTime(start=datetime.now())
-        }
+
+        self.credentials = credentials
+        self.process = process
+        self.notifiers = notifiers
+        self.today = DateInfo(date=today)
+        self.robot_time = RobotWorkTime(start=datetime.now())
 
     def is_work_day(self) -> bool:
-        today = self.args['today']
         scraper = CalendarScraper(
-            year=today.date.year,
-            backup_file=fr'C:\Users\robot.ad\Desktop\oper_day\resourses\{today.date.year}.html',
+            year=self.today.date.year,
+            backup_file=fr'C:\Users\robot.ad\Desktop\oper_day\resourses\{self.today.date.year}.html',
             session=self.session
         )
         date_infos: List[DateInfo] = scraper.date_infos
 
-        work_status = scraper.get_work_status(today=today.date, dates=date_infos)
+        work_status = scraper.get_work_status(today=self.today.date, dates=date_infos)
 
         i = 1
         while True:
-            date = today.date + timedelta(days=i)
+            date = self.today.date + timedelta(days=i)
             if scraper.get_work_status(today=date, dates=date_infos) == WorkStatus.WORK:
-                self.args['today'].next_work_date_str = DateInfo(date=date).date_str
+                self.today.next_work_date_str = DateInfo(date=date).date_str
                 break
             i += 1
 
         if work_status == WorkStatus.LONG:
-            self.args['today'] = DateInfo(date=today.date - timedelta(days=1), is_work_day=False)
+            self.today = DateInfo(date=self.today.date - timedelta(days=1), is_work_day=False)
 
         return True if work_status in (WorkStatus.WORK, WorkStatus.LONG) else False
+
+    @staticmethod
+    def emergency_call() -> None:
+        oracledb.init_oracle_client()
+        params: Dict[str, Any] = {
+            'user': os.getenv('ORACLE_USR'),
+            'password': os.getenv('ORACLE_PSW'),
+            'host': os.getenv('ORACLE_HOST'),
+            'port': os.getenv('ORACLE_PORT'),
+            'service_name': 'GCTI8TST',
+            'config_dir': r'C:\app\client\robot.ad\product\12.2.0\client_1\network\admin',
+        }
+        with oracledb.connect(**params) as connection:
+            cursor = connection.cursor()
+
+            last_record_id: int = cursor.execute(
+                'SELECT RECORD_ID FROM ocs.opovesheniye_robotom_po_colvir ORDER BY RECORD_ID DESC').fetchone()[0]
+            columns: str = 'RECORD_ID, CONTACT_INFO, CONTACT_INFO_TYPE, RECORD_TYPE, RECORD_STATUS,' \
+                           'CALL_RESULT, ATTEMPT, DAILY_FROM, DAILY_TILL, TZ_DBID, CHAIN_ID, CHAIN_N'
+            phone_number: str = '988079000000'
+
+            cursor.execute(f'''
+                INSERT INTO ocs.opovesheniye_robotom_po_colvir ({columns})
+                VALUES ({last_record_id + 1}, {phone_number}, 1, 2, 1, 28, 0, 39600, 72000, 134, 60, 0)
+            ''')
+
+            connection.commit()
 
     def run(self) -> None:
         if not self.is_work_day():
             pass
-            # self.args['notifier'].send_message(message='Не рабочий день. Завершаем работу.')
+            # self.args['notifiers'].log.send_message(message='Не рабочий день. Завершаем работу.')
             # return
 
         self.utils.kill_all_processes(proc_name='COLVIR', restricted_pids=self.restricted_pids)
-        colvir: Colvir = Colvir(**self.args)
-        colvir.run()
+
+        colvir: Colvir = Colvir(
+            credentials=self.credentials,
+            process=self.process,
+            today=self.today,
+            robot_time=self.robot_time,
+            notifiers=self.notifiers
+        )
+        try:
+            colvir.run()
+        except (RuntimeError, Exception) as e:
+            self.notifiers.alert.send_message(message='Не удалось запустить Colvir')
+            self.emergency_call()
+            raise e
+
         self.session.close()
