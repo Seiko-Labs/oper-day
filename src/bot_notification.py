8ed1e060a57ba35624a282af23c82990de5c5b3b
Alikhan Bissakov <bissakov@yandex.ru>
2023-02-15 12:29:29 +0600

    Update bot_notification.py

--- src/bot_notification.py
+++ src/bot_notification.py
@@ -1,13 +1,10 @@
-import os
-
-import dotenv
 import requests
 from requests.adapters import HTTPAdapter
 
 
 class TelegramNotifier:
-    def __init__(self, chat_id: str, session: requests.Session, token: str = None, retries: int = 5):
-        self.token: str = os.getenv('TOKEN') if not token else token
+    def __init__(self, token: str, chat_id: str, session: requests.Session, retries: int = 5):
+        self.token: str = token
         self.api_params = {'chat_id': chat_id, 'parse_mode': 'Markdown'}
         self.retries = retries
         self.session = session
@@ -20,3 +17,22 @@ class TelegramNotifier:
         if is_document:
             args['files'] = {'document': open(file=message, mode='rb')}
         return self.session.post(**args)
+
+
+if __name__ == '__main__':
+    import dotenv
+    from data_structures import Notifiers
+    import os
+
+    dotenv.load_dotenv()
+    with requests.Session() as session:
+        notifiers = Notifiers(
+            log=TelegramNotifier(token=os.getenv('TOKEN_LOG'), chat_id=os.getenv(f'CHAT_ID_LOG'), session=session),
+            alert=TelegramNotifier(token=os.getenv('TOKEN_ALERT'), chat_id=os.getenv(f'CHAT_ID_ALERT'), session=session)
+        )
+
+        # notifiers.log.send_message(message='Log test message from bot')
+        # notifiers.log.send_message(message=r'C:\Users\robot.ad\Desktop\2207_09.xlsb', is_document=True)
+
+        notifiers.alert.send_message(message='Alert test message from bot')
+        notifiers.alert.send_message(message=r'C:\Users\robot.ad\Desktop\2207_09.xlsb', is_document=True)
