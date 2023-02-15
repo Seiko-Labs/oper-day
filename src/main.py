29ca2a9cf16c638f7c86ad482b5828a28cf047aa
Alikhan Bissakov <bissakov@yandex.ru>
2023-02-15 13:13:42 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,34 +1,10 @@
 import os
 import warnings
-from typing import List
 import dotenv
 import requests
-from datetime import datetime, timedelta
-from data_structures import Credentials, Process, Notifiers, DateInfo, WorkStatus
+from data_structures import Credentials, Process, Notifiers
 from robot import Robot
 from bot_notification import TelegramNotifier
-from work_calendar import CalendarScraper
-
-
-def get_work_status(today: DateInfo, session: requests.Session) -> int:
-    scraper = CalendarScraper(
-        year=today.date.year,
-        backup_file=fr'C:\Users\robot.ad\Desktop\oper_day\resourses\{today.date.year}.html',
-        session=session
-    )
-    date_infos: List[DateInfo] = scraper.date_infos
-
-    work_status = scraper.get_work_status(today=today.date, dates=date_infos)
-
-    i = 1
-    while True:
-        date = today.date + timedelta(days=i)
-        if scraper.get_work_status(today=date, dates=date_infos) == WorkStatus.WORK:
-            today.next_work_date_str = DateInfo(date=date).date_str
-            break
-        i += 1
-
-    return work_status
 
 
 def main() -> None:
@@ -41,25 +17,17 @@ def main() -> None:
     with requests.Session() as session:
         credentials = Credentials(usr=colvir_usr, psw=colvir_psw)
         process = Process(name=process_name, path=process_path)
-        today = DateInfo(datetime.now().date())
         notifiers = Notifiers(
             log=TelegramNotifier(token=os.getenv('TOKEN_LOG'), chat_id=os.getenv(f'CHAT_ID_LOG'), session=session),
             alert=TelegramNotifier(token=os.getenv('TOKEN_ALERT'), chat_id=os.getenv(f'CHAT_ID_ALERT'), session=session)
         )
-
-        work_status = get_work_status(today=today, session=session)
-        if work_status == WorkStatus.LONG:
-            today = DateInfo(date=today.date - timedelta(days=1), is_work_day=False)
-        if work_status == WorkStatus.HOLIDAY:
-            notifiers.log.send_message(message='Не рабочий день. Завершаем работу.')
-            # return
 
         # notifiers.log.send_message('Робот начинает работу.')
         robot: Robot = Robot(
             credentials=credentials,
             process=process,
             notifiers=notifiers,
-            today=today
+            session=session
         )
         robot.run()
 
